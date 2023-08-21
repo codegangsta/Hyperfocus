156995788d8ec2817d874fc7fe63048398835e28
Jeremy Saenz <jeremy.saenz@gmail.com>
2023-08-20 18:47:43 -0700

    Initial commit

--- Hyperfocus/ContentView.swift
+++ Hyperfocus/ContentView.swift
@@ -7,84 +7,80 @@
 
 import SwiftUI
 import CoreData
+import ManagedSettings
+import FamilyControls
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    let wordDuration = 25 * 60
+    let breakDuration = 5 * 60
+    
+    let store = ManagedSettingsStore()
+        
+    @State private var timeRemaining: Int = 25 * 60
+    @State private var timer: Timer? = nil
+    @State private var isWorkTime = true
+    @State private var isBlocked = false
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
+        VStack(spacing: 20) {
+            Text(isWorkTime ? "Focus" : "Break")
+                .font(.largeTitle)
+            Text("\(timeRemaining / 60):\(String(format: "%02d", timeRemaining % 60))")
+                .font(.largeTitle)
+            
+            HStack {
+                Button("Start") {
+                    startTimer()
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-#if os(iOS)
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-#endif
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
+                Button("Stop") {
+                    stopTimer()
                 }
             }
-            Text("Select an item")
+            Button(isBlocked ? "Unblock" : "Block") {
+                toggleBlock()
+            }
         }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        .padding(.all)
+        .onAppear {
+            Task {
+                try await AuthorizationCenter.shared.requestAuthorization(for: FamilyControlsMember.individual)
             }
         }
     }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+    
+    private func toggleBlock() {
+        isBlocked.toggle()
+        
+        if isBlocked {
+            store.shield.applicationCategories = .all()
+            
+        } else {
+            store.shield.applicationCategories = .none
+        }
+        
+    }
+    
+    private func startTimer() {
+        stopTimer()
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            if(timeRemaining > 0) {
+                timeRemaining -= 1
+            } else {
+                isWorkTime.toggle()
+                timeRemaining = isWorkTime ? wordDuration : breakDuration
             }
         }
+    }
+    
+    private func stopTimer() {
+        timer?.invalidate()
+        timer = nil
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
